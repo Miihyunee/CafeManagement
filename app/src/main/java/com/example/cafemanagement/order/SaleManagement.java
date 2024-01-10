757e652ffe7f50f476a29a836b99b53ca51f2b6e
Mihyunee <esther2187@naver.com>
2024-01-10 12:12:00 +0900

    uploadSucess

---
+++ app/src/main/java/com/example/cafemanagement/order/SaleManagement.java
@@ -0,0 +1,49 @@
+package com.example.cafemanagement.order;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.cafemanagement.R;
+
+public class SaleManagement extends AppCompatActivity {
+
+    Button btnOrders;
+    Button btnDay;
+    Button btnMonth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sale_management);
+
+        btnOrders = findViewById(R.id.btnOrders);
+        btnDay = findViewById(R.id.btnDay);
+        btnMonth = findViewById(R.id.btnMonth);
+
+        btnOrders.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SaleManagement.this, OrdersActivity.class);
+                startActivity(intent);
+            }
+        });
+        btnDay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SaleManagement.this, DaySalemana.class);
+                startActivity(intent);
+            }
+        });
+        btnMonth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SaleManagement.this, MonthSalemana.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
